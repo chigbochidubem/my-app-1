f79d78649c4e818dcf8e5f2d39f561a77cb230e0
chigbochidubem <chigbodaniel7@gmail.com>
2025-05-21 10:31:38 +0100

    first commit

---
+++ config/bd.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (cached.promise) {
+    return cached.promise;
+  }
+
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/ecommerce`, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+export default dbConnect;
